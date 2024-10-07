7e328cf53852cf8987e4e6a8ec34d1f5e258285c
Vanikiller <divinethankgod227@gmail.com>
2024-10-08 00:19:22 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "50931461936"
-global.ownername = "🌹⃟⃢👑 ĐÆ𝚪𝐊༒𝙎-𝙏𝞢𝞜🌹⃟⃢👑"
+global.ownernomer = "2347070492951"
+global.ownername = "VAINKILLER "
 global.ytname = "DRK_ST_Tech"
 global.socialm = "GitHub: DRK-S-TEN"
 global.location = "America, Haiti, Port-au-Prince"
 
-global.ownernumber = '50931461936'  //creator number
-global.ownername = '𝙎-𝙏𝞢𝞜' //owner name
+global.ownernumber = '2347070492951'  //creator number
+global.ownername = 'VAINKILLER' //owner name
 global.botname = 'DRK_ST_BUG' //name of the bot
 
 //sticker details
